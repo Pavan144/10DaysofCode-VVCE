53852bee91fdf38fd830ad697a5b2a66a08ec117
Pavan Kumar D S <55590366+Pavan144@users.noreply.github.com>
2020-08-24 20:54:46 +0530

    Day-7 challENGE uploaded !!!

---
+++ Day-7 challENGE.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countSort function below.
+def countSort(n, arr): 
+
+    for l in arr:
+        l[0] = int(l[0]) 
+				
+    for i in range(n//2): 
+        arr[i][1] = "-"
+		
+    output = [[] for _ in range(n)]
+    for l in arr:
+        output[l[0]].append(l[1])
+        
+    print(' '.join(j for i in output for j in i))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(input().rstrip().split())
+
+    countSort(n,arr)
